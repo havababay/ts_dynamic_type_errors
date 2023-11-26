bfb6f19313ac9717fad3c89c59c9a1376d90450f
havababay <hava.babay@gmail.com>
2023-11-26 11:22:48 +0200

    add ts file showing the compilation errors due to strong type

---
+++ src/strong_type_demo.ts
@@ -0,0 +1,31 @@
+/*
+// Adding number to a string
+//==========================
+const num = 42;
+const str = "10";
+const result = num + str;
+*/
+
+/*
+// Multiply number with a string
+// =============================
+const num = 42;
+const str = "10";
+const result = num * str;
+*/
+
+/*
+// Multiply number with a string
+// =============================
+const num = 42;
+const str = "Hello";
+const result = num * str;
+*/
+
+/*
+// sqrt
+// =============================
+let num = 42;
+num = "Hello";
+const result = Math.sqrt(num);
+*/
